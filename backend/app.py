280df5bf9c2a15d81328e02686de59ed3363efac
Yael <srjean21@gmail.com>
2025-06-25 16:48:09 -0400

    (adds) Se añadió la configuración para activar las CORS

--- backend/app.py
+++ backend/app.py
@@ -1,20 +1,15 @@
 import sys
 from webview import create_window, start
 from flask import Flask
-
-app = Flask(__name__, static_folder="estatico", static_url_path="/")
+from flask_cors import CORS
 
 ARGS = sys.argv[1:]
 PREVIEW = "--preview" in ARGS
 DEBUG = "--debug" in ARGS
 
-# se usa la página estática al haberse compilado o usarse el argumento --preview
-if PREVIEW or getattr(sys, "frozen", False):
-
-    @app.route("/<path:path>")
-    @app.route("/")
-    def root(path="/"):
-        return app.send_static_file("index.html")
+app = Flask(__name__)
+CORS(app)
+app.config["CORS_HEADERS"] = "Content-Type"
 
 
 if __name__ == "__main__":
