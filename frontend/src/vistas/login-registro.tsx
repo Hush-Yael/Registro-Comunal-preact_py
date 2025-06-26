8cb58f0a477e46375d0b0eeabd6566dd7cf87863
Yael <srjean21@gmail.com>
2025-06-26 12:14:46 -0400

    (adds) Se añadió la vista para el login y el registro.

---
+++ frontend/src/vistas/login-registro.tsx
@@ -0,0 +1,63 @@
+import { useContext } from "preact/hooks";
+import Cabecera from "../componentes/cabecera";
+import Input from "../componentes/formulario/input";
+import Formulario, {
+  contextoFormulario,
+  MensajeCarga,
+  MensajeExito,
+} from "../componentes/formulario";
+import Contraseña from "../componentes/formulario/contraseña";
+import { useLocation } from "preact-iso";
+import { sesion } from "..";
+
+export default () => {
+  const path = useLocation().path;
+  const LOGIN = path == "/login";
+
+  return (
+    <>
+      <Cabecera
+        titulo={LOGIN ? "Inicio de sesión" : "Registrar nuevo usuario"}
+      />
+      <Formulario
+        rutaApi={LOGIN ? "login" : "registro"}
+        datos={{ nombre: "", contraseña: "" }}
+        onSuccess={({ contexto, json }) => {
+          if (LOGIN) {
+            sesion.value = {
+              usuario: contexto.datos.value.nombre,
+              rol: json,
+            };
+          }
+        }}
+      >
+        <Datos />
+      </Formulario>
+    </>
+  );
+};
+
+const Datos = () => {
+  const path = useLocation().path;
+  const LOGIN = path == "/login";
+  const { estado } = useContext(contextoFormulario);
+
+  return (
+    <div class="col mt-16 gap-6">
+      <Input campo="nombre" id="nombre-de-usuario" label="Nombre de usuario" />
+      <Contraseña />
+
+      <MensajeCarga texto={LOGIN ? "Verificando datos..." : "Registrando..."} />
+      {!LOGIN && <MensajeExito texto="Usuario registrado con éxito" />}
+
+      <div class="col gap-1.5">
+        <button class="btn btn-primario" disabled={estado.value == "subiendo"}>
+          {LOGIN ? "Entrar" : "Registrar"}
+        </button>
+        <a href={LOGIN ? "/registro" : "/login"} class="btn btn-secundario">
+          Ir al {LOGIN ? "registro" : "inicio de sesión"}
+        </a>
+      </div>
+    </div>
+  );
+};
