ae8286de65a2974401f4c4de1e348315aa7cfad8
Yael <srjean21@gmail.com>
2025-06-27 17:18:15 -0400

    (adds) Se adaptó la tabla para que muestre unos controles al usar la paginación.

--- frontend/src/componentes/tabla.tsx
+++ frontend/src/componentes/tabla.tsx
@@ -10,6 +10,7 @@ import {
 import { useEffect } from "preact/hooks";
 import type { JSX } from "preact/jsx-runtime";
 import Carga from "./carga";
+import Iconos from "./iconos";
 
 declare module "@tanstack/react-table" {
   interface TableMeta<TData extends RowData> {
@@ -61,49 +62,128 @@ export default <
   });
 
   return (
-    <Tabla wrapperClass={props.wrapperClass} class={props.class}>
-      <thead>
-        {tabla.getHeaderGroups().map((headerGroup) => (
-          <tr key={headerGroup.id}>
-            {headerGroup.headers.map((header) => (
-              <Cabecera key={header.id} style={{ width: header.getSize() }}>
-                {header.isPlaceholder
-                  ? null
-                  : flexRender(
-                      header.column.columnDef.header,
-                      header.getContext()
-                    )}
-              </Cabecera>
-            ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody>
-        {props.fetchValues && !cargado.value ? (
-          <tr class="bg-neutral-200">
-            <td class="p-4" colSpan={props.columnas.length}>
-              <span
-                role="status"
-                className="flex items-center justify-center gap-2"
-              >
-                <Carga class="size-5 mx-1" />
-                Cargando...
-              </span>
-            </td>
-          </tr>
-        ) : (
-          tabla.getRowModel().rows.map((row) => (
-            <Fila key={row.id}>
-              {row.getVisibleCells().map((cell) => (
-                <td key={cell.id} class="p-1.5 px-2">
-                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                </td>
+    <>
+      <Tabla wrapperClass={props.wrapperClass} class={props.class}>
+        <thead>
+          {tabla.getHeaderGroups().map((headerGroup) => (
+            <tr key={headerGroup.id}>
+              {headerGroup.headers.map((header) => (
+                <Cabecera key={header.id} style={{ width: header.getSize() }}>
+                  {header.isPlaceholder
+                    ? null
+                    : flexRender(
+                        header.column.columnDef.header,
+                        header.getContext()
+                      )}
+                </Cabecera>
               ))}
-            </Fila>
-          ))
-        )}
-      </tbody>
-    </Tabla>
+            </tr>
+          ))}
+        </thead>
+        <tbody>
+          {props.fetchValues && !cargado.value ? (
+            <tr class="bg-neutral-200">
+              <td class="p-4" colSpan={props.columnas.length}>
+                <span
+                  role="status"
+                  class="flex items-center justify-center gap-2"
+                >
+                  <Carga class="size-5 mx-1" />
+                  Cargando...
+                </span>
+              </td>
+            </tr>
+          ) : (
+            tabla.getRowModel().rows.map((row) => (
+              <Fila key={row.id}>
+                {row.getVisibleCells().map((cell) => (
+                  <td key={cell.id} class="p-1.5 px-2">
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </td>
+                ))}
+              </Fila>
+            ))
+          )}
+        </tbody>
+      </Tabla>
+      {props.options?.getPaginationRowModel && (
+        <div class="flex items-center justify-end gap-5 w-full mt-4">
+          <span role="status" class="flex items-center gap-1">
+            <div>Página</div>
+            <strong>
+              {tabla.getState().pagination.pageIndex + 1} de{" "}
+              {tabla.getPageCount().toLocaleString()}
+            </strong>
+          </span>
+          <fieldset class="flex items-center gap-1.5">
+            <button
+              class="rounded bg-neutral-200"
+              onClick={() => tabla.firstPage()}
+              disabled={!tabla.getCanPreviousPage()}
+            >
+              <Iconos.DobleFlechaDer class="size-6 transform-[rotate(180deg)]" />
+            </button>
+            <button
+              class="rounded bg-neutral-200"
+              onClick={() => tabla.previousPage()}
+              disabled={!tabla.getCanPreviousPage()}
+            >
+              <Iconos.FlechaDer class="size-6 transform-[rotate(180deg)]" />
+            </button>
+            <button
+              class="rounded bg-neutral-200"
+              onClick={() => tabla.nextPage()}
+              disabled={!tabla.getCanNextPage()}
+            >
+              <Iconos.FlechaDer class="size-6" />
+            </button>
+            <button
+              class="rounded bg-neutral-200"
+              onClick={() => tabla.lastPage()}
+              disabled={!tabla.getCanNextPage()}
+            >
+              <Iconos.DobleFlechaDer class="size-6" />
+            </button>
+          </fieldset>
+          <label class="flex items-center gap-2" htmlFor="page-size">
+            <span className="min-w-max">Filas por página:</span>
+            <select
+              id="page-size"
+              class="w-full p-0.5 px-1 rounded bg-neutral-200 text-neutral-700"
+              value={tabla.getState().pagination.pageSize}
+              onChange={(e) => {
+                tabla.setPageSize(Number((e.target as HTMLInputElement).value));
+              }}
+            >
+              {Array.from({ length: 10 }, (_, i) => (i + 1) * 10)
+                .concat([200, 500, 1000])
+                .map((pageSize) => (
+                  <option key={pageSize} value={pageSize}>
+                    {pageSize}
+                  </option>
+                ))}
+            </select>
+          </label>
+          <label htmlFor="page" class="flex items-center gap-2">
+            <span className="min-w-max">Ir a la página:</span>
+            <input
+              class="w-full p-0.5 px-1 rounded bg-neutral-200 text-neutral-700"
+              type="number"
+              min="1"
+              id="page"
+              max={tabla.getPageCount()}
+              value={tabla.getState().pagination.pageIndex + 1}
+              onChange={(e) => {
+                const page = (e.target as HTMLInputElement).value
+                  ? Number((e.target as HTMLInputElement).value) - 1
+                  : 0;
+                tabla.setPageIndex(page);
+              }}
+            />
+          </label>
+        </div>
+      )}
+    </>
   );
 };
 
@@ -127,7 +207,7 @@ export const Tabla = (
 export const Cabecera = (props: JSX.IntrinsicElements["th"]) => (
   <th
     {...props}
-    class={`sticky top-0 p-1.5 px-2 bg-neutral-900 text-white border-b border-neutral-600 first:rounded-tl-md last:rounded-tr-md text-nowrap ${
+    class={`sticky top-0 p-1.5 px-2 bg-neutral-900 text-white first:rounded-tl-md last:rounded-tr-md text-nowrap ${
       props.class || ""
     }`}
   >
