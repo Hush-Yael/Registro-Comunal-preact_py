a20e7a2a4c2a38f4cc15df47a3ed30ecba2af3fa
Yael <srjean21@gmail.com>
2025-06-30 23:03:20 -0400

    (changes & adds) Se adaptó la app para que la webview use el servidor de Flask para servir el index.html, en lugar de usar el suyo propio, ya que este fallaba al intentar hacer peticiones al backend.

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,7 @@
 import sys
-import os
 from webview import create_window, start, settings
 from threading import Thread, Event
-from flask import Flask
-from flask_cors import CORS
-from constantes import RUTA_BASE
+from flask import Flask, request
 from api.index import api
 
 
@@ -13,14 +10,35 @@ PREVIEW = "--preview" in ARGS or "-P" in ARGS
 DEV = "--dev" in ARGS or "-D" in ARGS
 
 
-app = Flask(__name__)
-CORS(app)
+app = Flask(
+    __name__,
+    static_folder="estatico",
+    template_folder="estatico",
+    static_url_path="/",
+)
 app.config["CORS_HEADERS"] = "Content-Type"
+app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 0
 
 app.register_blueprint(api)
 
 
-def correr_backend():
+@app.route("/", defaults={"path": ""})
+@app.route("/<path:path>")
+def catch_all(path):
+    return app.send_static_file("index.html")
+
+
+# se evita el error 404 cuando se intenta acceder a un archivo no existente, ya que siempre se debería mostrar el index.html
+@app.errorhandler(404)
+def handle_404(e):
+    # no es una petición de api
+    if request.method == "GET" and not request.url.startswith("/api"):
+        return app.send_static_file("index.html")
+    # es una petición que debe fallar al no existir el recurso realmente
+    return e
+
+
+def correr_servidor():
     app.run(host="0.0.0.0" if DEV else "localhost", debug=DEV, port=1144)
 
 
@@ -29,7 +47,7 @@ def iniciar_flask(evento_listo):
         # Señal que el backend se ha iniciado
         evento_listo.set()
         # Se inicia el servidor
-        correr_backend()
+        correr_servidor()
     except Exception as e:
         print(f"Configuracion del backend fallida: {str(e)}")
         evento_listo.set()
@@ -40,7 +58,7 @@ def iniciar_flask(evento_listo):
 settings["ALLOW_DOWNLOADS"] = True
 
 if __name__ == "__main__":
-    # se corre con la webview al usar el ejecutable o al usar el argumento --preview
+    # se corre con la webview al usar el ejecutable o al usar estar en el modo PREVIEW
     if PREVIEW or getattr(sys, "frozen", False):
         # Se crea un evento para indicar que el inicio del backend se ha completado
         backend_listo = Event()
@@ -58,15 +76,14 @@ if __name__ == "__main__":
 
         create_window(
             "Registro Comunal",
-            # La app compilada busca el index.html en la carpeta estática, en modo debug se usa el servidor de Vite
-            url=os.path.join(RUTA_BASE, "estatico", "index.html")
-            if not DEV
-            else "http://localhost:5173",
+            # La app compilada usa la url de Flask que sirve el html desde la carpeta estática, en modo desarrollo se usa la del servidor de Vite
+            server=app,  # type: ignore
+            url="http://localhost:1144" if not DEV else "http://localhost:5173",
             text_select=True,
             zoomable=True,
         )
 
-        start(debug=DEV, private_mode=False)
+        start(debug=True, private_mode=False)
     # se inicia el servidor normalmente, para desarrollo
     else:
-        correr_backend()
+        correr_servidor()
