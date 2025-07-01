b166b1684f56a04ef3c3161b58328a4b6a5f373f
Yael <srjean21@gmail.com>
2025-06-30 23:29:07 -0400

    (changes) Se evita hacer la build en la carpeta del backend, ya que los archivos estáticos se deben extraer de aquí solo en producción, y eso se debe hacer automáticamente con .spec

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,11 @@
-import { defineConfig } from 'vite';
-import preact from '@preact/preset-vite';
-import tailwindcss from '@tailwindcss/vite'
-
+import { defineConfig } from "vite";
+import preact from "@preact/preset-vite";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-	plugins: [preact(), tailwindcss()],
-	server: { host: true },
-	build: {
-		"target": "es2020",
-		emptyOutDir: true,
-		// servir el bundle en el backend
-		"outDir": "../backend/estatico"
-	}
+  plugins: [preact(), tailwindcss()],
+  server: { host: true },
+  build: {
+    target: "es2020",
+  },
 });
