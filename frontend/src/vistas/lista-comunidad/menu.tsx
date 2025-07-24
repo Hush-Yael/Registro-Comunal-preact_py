74723bcac235e8a73db9c6d9bad31585857e8de3
Yael <srjean21@gmail.com>
2025-07-24 17:06:00 -0400

    (changes & adds) Se añadió la librería "radix-ui", para cambiar los <details> del menú de opciones de tabla de la comunidad por unos <DropdownMenu>

--- frontend/src/vistas/lista-comunidad/menu.tsx
+++ frontend/src/vistas/lista-comunidad/menu.tsx
@@ -1,6 +1,5 @@
 import { type Table } from "@tanstack/react-table";
 import { signal } from "@preact/signals";
-import { useRef } from "preact/hooks";
 import Iconos from "~/componentes/iconos";
 import type { DatosComunidad, OrdenKey } from "~/tipos";
 import { sesion } from "~/index";
@@ -11,117 +10,78 @@ import {
   datosComunidad,
   ordenColumnas,
 } from "./contantes";
+import { DropdownMenu as Menu } from "radix-ui";
 
 const _portando = signal(false);
-let subOpenTimeout: number;
-let subCloseTimeout: number;
 
 export default (props: { tabla: Table<DatosComunidad> }) => {
-  const submenu = useRef<HTMLDetailsElement>();
-
-  const closeSub = () => {
-    clearTimeout(subOpenTimeout);
-    clearTimeout(subCloseTimeout);
-
-    subCloseTimeout = setTimeout(
-      () => submenu.current.removeAttribute("open"),
-      200
-    );
-  };
-
   return (
-    <details onMouseLeave={closeSub} class="group relative">
-      <summary class="list-none btn p-1! px-0! group-open:bg-primary! group-open:text-[var(--bg-base)]! group-open:border-transparent! cursor-pointer">
+    <Menu.Root>
+      <Menu.Trigger class="list-none btn p-1! px-0! data-[state=open]:bg-primary! data-[state=open]:text-[var(--bg-base)]! data-[state=open]:border-transparent! cursor-pointer">
         <Iconos.Menu class="size-6" />
         <span className="sr-only">Opciones</span>
-      </summary>
+      </Menu.Trigger>
 
-      <div class="dropdown-content right-0 origin-top-right" role="group">
-        <details ref={submenu} class="relative">
-          <summary
-            class="dropdown-item gap-0! list-none cursor-default"
-            onMouseEnter={() => {
-              clearTimeout(subCloseTimeout);
-              clearTimeout(subOpenTimeout);
+      <Menu.Portal>
+        <Menu.Content
+          onInteractOutside={(e) =>
+            (e.target as HTMLElement).closest("[data-radix-menu-content]") &&
+            e.preventDefault()
+          }
+          align="start"
+          side="right"
+          sideOffset={5}
+          alignOffset={-2}
+          class="dropdown-content"
+        >
+          <Orden />
 
-              subOpenTimeout = setTimeout(() => {
-                submenu.current.setAttribute("open", "");
-              }, 300);
+          <Menu.Item
+            class="dropdown-item"
+            onSelect={() => {
+              cargarDatosComunidad.value = true;
             }}
-            onMouseLeave={closeSub}
+            disabled={cargarDatosComunidad.value === true || _portando.value}
           >
-            <Iconos.FlechaDer class="size-6 transform-[rotate(180deg)_translate(3px,1px)]" />
-            Orden de los datos
-          </summary>
+            <Iconos.Recargar />
+            Recargar datos
+          </Menu.Item>
 
-          <div
-            class="dropdown-content -top-2 origin-right max-[440px]:-left-35 min-[440px]:right-[107%]"
-            onMouseEnter={() => clearTimeout(subCloseTimeout)}
-            onMouseLeave={closeSub}
+          <Menu.Item
+            class="dropdown-item"
+            // @ts-expect-error: no importa
+            onSelect={props.tabla.resetColumnFilters}
           >
-            <h2 class="font-bold text-center py-1">Ordenar datos por:</h2>
+            <Iconos.LimpiarFiltros />
+            Limpiar filtros
+          </Menu.Item>
 
-            <div class="col gap-2.5" role="group">
-              <SubItem id="nombres">Nombres</SubItem>
+          <Menu.Item
+            class="dropdown-item"
+            onSelect={exportar}
+            disabled={_portando.value || datosComunidad.value.length < 1}
+          >
+            <Iconos.Exportar />
+            Exportar datos...
+          </Menu.Item>
 
-              <SubItem id="apellidos">Apellidos</SubItem>
-
-              <SubItem id="cedula">Cedula</SubItem>
-
-              <SubItem id="fecha_nacimiento">Fecha de nacimiento</SubItem>
-
-              <SubItem id="numero_casa">Número de casa</SubItem>
-
-              <SubItem id="rowid">Orden de añadido</SubItem>
-
-              <SubItem id="editado">Fecha de edición</SubItem>
-            </div>
-          </div>
-        </details>
-
-        <button
-          class="dropdown-item"
-          onClick={() => {
-            cargarDatosComunidad.value = true;
-          }}
-          disabled={cargarDatosComunidad.value === true || _portando.value}
-        >
-          <Iconos.Recargar />
-          Recargar datos
-        </button>
-
-        <button
-          class="dropdown-item"
-          // @ts-expect-error: no importa
-          onClick={props.tabla.resetColumnFilters}
-        >
-          <Iconos.LimpiarFiltros />
-          Limpiar filtros
-        </button>
-
-        <button
-          class="dropdown-item"
-          onClick={exportar}
-          disabled={_portando.value || datosComunidad.value.length < 1}
-        >
-          <Iconos.Exportar />
-          Exportar datos...
-        </button>
-
-        <input
-          class="peer/input sr-only"
-          onChange={importar}
-          disabled={sesion.value.rol !== "admin" || _portando.value}
-          type="file"
-          accept=".csv"
-          id="exportar"
-        />
-        <label class="dropdown-item cursor-pointer" htmlFor="exportar">
-          <Iconos.Importar />
-          Importar datos...
-        </label>
-      </div>
-    </details>
+          <input
+            class="peer/input sr-only"
+            onChange={importar}
+            disabled={sesion.value.rol !== "admin" || _portando.value}
+            type="file"
+            accept=".csv"
+            id="exportar"
+          />
+          <Menu.Item asChild>
+            <label class="dropdown-item cursor-pointer" htmlFor="exportar">
+              <Iconos.Importar />
+              Importar datos...
+            </label>
+          </Menu.Item>
+        </Menu.Content>
+      </Menu.Portal>
+    </Menu.Root>
   );
 };
 
@@ -174,75 +134,99 @@ const exportar = async () => {
   _portando.value = false;
 };
 
-type SubItemProps = Omit<JSX.IntrinsicElements["input"], "type" | "id"> & {
+const Orden = () => {
+  return (
+    <Menu.Sub defaultOpen>
+      <Menu.SubTrigger class="dropdown-item gap-0! list-none cursor-default">
+        <Iconos.FlechaDer class="size-6 transform-[rotate(180deg)_translate(3px,1px)]" />
+        Orden de los datos
+      </Menu.SubTrigger>
+
+      <Menu.Portal>
+        <Menu.SubContent
+          sideOffset={10}
+          collisionPadding={10}
+          class="dropdown-content max-[375px]:transform-[translateX(50%)]! max-[450px]:transform-[translateX(40%)]!"
+        >
+          <Menu.Label class="font-bold text-center py-1">
+            Ordenar datos por:
+          </Menu.Label>
+
+          <div class="col gap-2.5" role="group">
+            <OrdenItem id="nombres">Nombres</OrdenItem>
+
+            <OrdenItem id="apellidos">Apellidos</OrdenItem>
+
+            <OrdenItem id="cedula">Cedula</OrdenItem>
+
+            <OrdenItem id="fecha_nacimiento">Fecha de nacimiento</OrdenItem>
+
+            <OrdenItem id="numero_casa">Número de casa</OrdenItem>
+
+            <OrdenItem id="rowid">Orden de añadido</OrdenItem>
+
+            <OrdenItem id="editado">Fecha de edición</OrdenItem>
+          </div>
+        </Menu.SubContent>
+      </Menu.Portal>
+    </Menu.Sub>
+  );
+};
+
+type OrdenItemProps = Omit<JSX.IntrinsicElements["input"], "type" | "id"> & {
   id: OrdenKey;
 };
 
-const SubItem = (props: SubItemProps) => (
-  <div
-    class="flex items-center justify-between gap-6 w-full px-1"
-    role="group"
-    aria-labelledby={props.id + "-l"}
-  >
-    <p id={props.id + "-l"}>
-      <span className="sr-only">Ordenar por </span>
-      {props.children}
-    </p>
+const OrdenItem = (props: OrdenItemProps) => {
+  const idChecked = ordenColumnas.value[0] === props.id;
+  const descChecked = idChecked && ordenColumnas.value[1] === "desc";
+  const ascChecked = idChecked && !descChecked;
 
-    <div
-      class="flex items-center gap-1"
-      role="group"
-      aria-label="Dirección de orden"
-    >
-      <label>
-        <input
-          class="peer sr-only"
-          type="checkbox"
-          name={`${props.id}-orden`}
-          checked={
-            ordenColumnas.value[0] === props.id &&
-            ordenColumnas.value[1] === "asc"
-          }
-          onChange={() => {
-            ordenColumnas.value =
-              ordenColumnas.value[0] === props.id &&
-              ordenColumnas.value[1] === "asc"
-                ? []
-                : [props.id, "asc"];
-            cargarDatosComunidad.value = true;
-          }}
-        />
-        <span className="sr-only">ascendente</span>
+  return (
+    <Menu.Group class="flex items-center justify-between gap-6 w-full px-1">
+      <p id={props.id + "-l"}>
+        <span className="sr-only">Ordenar por </span>
+        {props.children}
+      </p>
 
-        <span class="dropdown-radio-btn">
-          <Iconos.Ascendente />
-        </span>
-      </label>
+      <Menu.Group
+        class="flex items-center gap-1"
+        aria-label="Dirección de orden"
+      >
+        <Menu.CheckboxItem
+          class="dropdown-check-btn"
+          checked={ascChecked}
+          onSelect={(e) => e.preventDefault()}
+          aria-label="ascendente"
+          asChild
+        >
+          <button
+            onClick={() => {
+              ordenColumnas.value = ascChecked ? [] : [props.id, "asc"];
+              cargarDatosComunidad.value = true;
+            }}
+          >
+            <Iconos.Ascendente />
+          </button>
+        </Menu.CheckboxItem>
 
-      <label>
-        <input
-          class="peer sr-only"
-          type="checkbox"
-          name={`${props.id}-orden`}
-          checked={
-            ordenColumnas.value[0] === props.id &&
-            ordenColumnas.value[1] === "desc"
-          }
-          onChange={() => {
-            ordenColumnas.value =
-              ordenColumnas.value[0] === props.id &&
-              ordenColumnas.value[1] === "desc"
-                ? []
-                : [props.id, "desc"];
-            cargarDatosComunidad.value = true;
-          }}
-        />
-        <span className="sr-only">descendente</span>
-
-        <span class="dropdown-radio-btn">
-          <Iconos.Descendente />
-        </span>
-      </label>
-    </div>
-  </div>
-);
+        <Menu.CheckboxItem
+          class="dropdown-check-btn"
+          checked={descChecked}
+          onSelect={(e) => e.preventDefault()}
+          aria-label="descendente"
+          asChild
+        >
+          <button
+            onClick={() => {
+              ordenColumnas.value = descChecked ? [] : [props.id, "desc"];
+              cargarDatosComunidad.value = true;
+            }}
+          >
+            <Iconos.Descendente />
+          </button>
+        </Menu.CheckboxItem>
+      </Menu.Group>
+    </Menu.Group>
+  );
+};
