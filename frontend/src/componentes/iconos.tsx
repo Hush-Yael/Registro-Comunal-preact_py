569b13bb4d439d993ac8118a1cda85d810d3280a
Yael <srjean21@gmail.com>
2025-07-26 15:42:41 -0400

    (change) Se cambió el ícono "Menu"

--- frontend/src/componentes/iconos.tsx
+++ frontend/src/componentes/iconos.tsx
@@ -497,17 +497,8 @@ export default {
   ),
 
   Menu: (props: JSX.IntrinsicElements["svg"]) => (
-    <svg
-      {...props}
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-    >
-      <circle cx="12" cy="12" r="1" />
-      <circle cx="12" cy="5" r="1" />
-      <circle cx="12" cy="19" r="1" />
+    <svg {...props} viewBox="0 -960 960 960">
+      <path d="M480-160q-33 0-56.5-23.5T400-240q0-33 23.5-56.5T480-320q33 0 56.5 23.5T560-240q0 33-23.5 56.5T480-160Zm0-240q-33 0-56.5-23.5T400-480q0-33 23.5-56.5T480-560q33 0 56.5 23.5T560-480q0 33-23.5 56.5T480-400Zm0-240q-33 0-56.5-23.5T400-720q0-33 23.5-56.5T480-800q33 0 56.5 23.5T560-720q0 33-23.5 56.5T480-640Z" />
     </svg>
   ),
 
