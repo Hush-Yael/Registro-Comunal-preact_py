7dcffc999ba2257c9f07d211abd7bb0ed8930353
Yael <srjean21@gmail.com>
2025-07-30 16:59:20 -0400

    (add) Se añadió un ícono de información

--- frontend/src/componentes/iconos.tsx
+++ frontend/src/componentes/iconos.tsx
@@ -580,4 +580,19 @@ export default {
       <circle cx="12" cy="12" r="3" />
     </svg>
   ),
+
+  Info: (props: JSX.IntrinsicElements["svg"]) => (
+    <svg
+      {...props}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    >
+      <circle cx="12" cy="12" r="10" />
+      <path d="M12 16v-4" />
+      <path d="M12 8h.01" />
+    </svg>
+  ),
 };
