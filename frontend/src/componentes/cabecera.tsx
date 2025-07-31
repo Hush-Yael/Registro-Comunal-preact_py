f42e3b88746c8de90aa9cf64052fce12f0c99eea
Yael <srjean21@gmail.com>
2025-07-31 13:37:17 -0400

    (change) Se redujo el espacio entre los dos encabezados

--- frontend/src/componentes/cabecera.tsx
+++ frontend/src/componentes/cabecera.tsx
@@ -2,7 +2,7 @@ import { ComponentChildren } from "preact";
 
 export default (props: { titulo: string; children?: ComponentChildren }) => (
   <header class="flex justify-between items-center gap-15 w-full max-sm:gap-7">
-    <div class="col gap-2">
+    <div class="col gap-1">
       <h1 class="font-bold text-xl max-sm:text-lg">{props.titulo}</h1>
       <h2 class="text-muted max-sm:text-sm">
         Comunidad Santo Domingo de Guzmán
