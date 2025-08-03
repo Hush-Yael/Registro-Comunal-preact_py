eefabc3ec273038075796bf5f3c82c01475facee
Yael <srjean21@gmail.com>
2025-08-03 13:25:10 -0400

    (fixes) Se corrigieron los tipos

--- frontend/src/tipos/lista-comunidad.d.ts
+++ frontend/src/tipos/lista-comunidad.d.ts
@@ -2,10 +2,7 @@ import { OPCIONES_FILTROS } from "~/constantes/lista-comunidad";
 import { DatosComunidad } from ".";
 
 export type FiltroKey =
-  | Exclude<
-      keyof DatosComunidad,
-      "nombres" | "apellidos" | "id" | "patologia" | "direccion"
-    >
+  | Exclude<keyof DatosComunidad, "nombres" | "apellidos" | "id" | "patologia">
   | "Nombres y apellidos";
 
 export type FiltroId<ColumnaId extends FiltroKey> =
@@ -13,6 +10,9 @@ export type FiltroId<ColumnaId extends FiltroKey> =
     ? keyof (typeof OPCIONES_FILTROS)[ColumnaId]
     : keyof (typeof OPCIONES_FILTROS)["comun"];
 
-export type OrdenKey = keyof DatosComunidad | "editado" | "rowid";
+export type OrdenKey =
+  | Exclude<keyof DatosComunidad, "id">
+  | "editado"
+  | "rowid";
 
 export type OrdenColumnas = [] | [OrdenKey, "asc" | "desc"];
